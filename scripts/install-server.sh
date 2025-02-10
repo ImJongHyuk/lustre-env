cad2c1d890fd71eb3151fd7bc92910e1ce3066da
Im Jong-Hyuk <imjhyuk@dnotitia.com>
2025-02-10 15:12:12 +0900

    Docker 관련 파일 업데이트.

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -8,17 +8,28 @@ if [[ ! -d "$LUSTRE_HOME" ]]; then
     return 1
 fi
 
+USER=$(whoami)
+if [ "$USER" = "root" ]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
 
 # Update package list
-sudo apt update
+$SUDO apt update
 
 # Install required packages in alphabetical order
-sudo apt install -y \
+$SUDO apt install -y \
     bison \
+    build-essential \
+    debhelper \
+    dkms \
     flex \
     libmount-dev \
     libnl-3-dev \
+    libnl-genl-3-dev \
     libopenmpi-dev \
+    libreadline-dev \
     libselinux1-dev \
     libssl-dev \
     libtool \
@@ -26,21 +37,37 @@ sudo apt install -y \
     libzfslinux-dev \
     module-assistant \
     pkg-config \
+    python3-dev \
+    python3-distutils \
+    python3-setuptools \
     quilt \
     swig \
     zfs-dkms \
     zfsutils-linux \
     zlib1g-dev
 
-
 LUSTRE_RELEASE_DIR=$LUSTRE_HOME/lustre-release
 
 # Check whether the Lustre repository is cloned.
 if [ -d "$LUSTRE_RELEASE_DIR/.git" ] && git -C "$LUSTRE_RELEASE_DIR" rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    echo "Lustre repository already exists at $LUSTRE_RELEASE_DIR, skipping clone."
+    echo "Lustre repository found at $LUSTRE_RELEASE_DIR; skipping clone."
 else
-    echo "Cloning Lustre repository... -> $LUSTRE_RELEASE_DIR"
-    git clone git://git.whamcloud.com/fs/lustre-release.git "$LUSTRE_RELEASE_DIR"
+    echo "Cloning Lustre repository at $LUSTRE_RELEASE_DIR..."
+    retries=0
+    max_retries=3
+    while true; do
+        if git clone git://git.whamcloud.com/fs/lustre-release.git "$LUSTRE_RELEASE_DIR"; then
+            break
+        else
+            retries=$((retries+1))
+            if [ "$retries" -ge "$max_retries" ]; then
+                echo "git clone failed after ${retries} attempts. Exiting."
+                exit 1
+            fi
+            echo "git clone failed. Retrying (${retries}/${max_retries})..."
+            sleep 5
+        fi
+    done
 fi
 
 cd $LUSTRE_RELEASE_DIR
